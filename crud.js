5e537fa3781674c794cb645d9dc083adaba078e9
phhernandes <pedrobeco97@gmail.com>
2023-05-16 09:17:13 -0300

    commit

--- crud.js
+++ crud.js
@@ -88,7 +88,7 @@ function createCard(tarefa) {
 
   return `
     <div class="col-lg-3 col-md-6 col-12">
-        <div class="card">
+        <div class="card mb-3">
           <div class="card-header">
             ${tarefa.titulo}
           </div>
